7e16164a01ae6a9f374f6ba4aaf3fe2f9c874bae
kitnutthakorn-wq <kitnutthakorn@gmail.com>
2026-03-25 22:37:30 +0700

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require("express");
 const crypto = require("crypto");
 const path = require("path");
 const { createClient } = require("@supabase/supabase-js");
-const fetch = require("node-fetch");
+await fetch("https://api.line.me/v2/bot/message/reply", {...})
 
 const app = express();
 const PORT = process.env.PORT || 3000;

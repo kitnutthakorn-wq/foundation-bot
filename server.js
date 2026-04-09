8d81a2d672f3c9b0701b9f6d3c5d65c34d46605c
kitnutthakorn-wq <kitnutthakorn@gmail.com>
2026-04-09 14:04:37 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1510,7 +1510,7 @@ function buildTeamMenuOverlayFlex() {
             contents: [
               {
                 type: "image",
-                url: "https://img2.pic.in.th/LineDesign2.png",
+                url: "https://img2.pic.in.th/New_WorkTeam.png",
                 size: "full",
                 aspectMode: "cover",
              

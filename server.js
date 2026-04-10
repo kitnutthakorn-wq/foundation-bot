2fd25a4a9a1edea54dd87291a2a0251b7c745708
kitnutthakorn-wq <kitnutthakorn@gmail.com>
2026-04-10 17:14:28 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1612,7 +1612,7 @@ function buildUrgentCasePosterImagemap(caseData, baseUrlOverride = "") {
 
   return {
     type: "imagemap",
-    baseUrl: `${baseUrl}/imagemap/urgent-case-poster?case_code=${caseData.case_code}`,
+    baseUrl: `${baseUrl}/imagemap/urgent-case-poster?case_code=${encodeURIComponent(caseData?.case_code || "")}`,
     altText: "ศูนย์ปฏิบัติการเคสด่วน",
     baseSize: {
       width: 1040,

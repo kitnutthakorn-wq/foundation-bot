8c2e2dcd522c030bb30d28025db2fc10a5844ce8
kitnutthakorn-wq <kitnutthakorn@gmail.com>
2026-04-11 17:21:56 +0700

    Update server.js

--- server.js
+++ server.js
@@ -274,7 +274,7 @@ drawText(ctx, priorityThai, INNER_X + 100, 640, {
     ctx.strokeStyle = "#D9D9D9";
     ctx.lineWidth = 2;
     ctx.beginPath();
-    ctx.moveTo(CARD.left + 34, 700);
+    ctx.moveTo(CARD.left + 34, 835);
     ctx.lineTo(CARD.left + CARD.width - 34, 835);
     ctx.stroke();
 

67055c6f5d5d2f83f981650e79b36ce0b91bffce
kitnutthakorn-wq <kitnutthakorn@gmail.com>
2026-04-12 23:10:20 +0700

    Update server.js

--- server.js
+++ server.js
@@ -644,19 +644,19 @@ app.get("/imagemap/urgent-case-menu-v2-r:rev/:size", async (req, res) => {
     const centerX = 520;
 
     // 🔥 ปุ่ม 1
-    drawText(ctx, `เคสด่วน SLA วิกฤต (${counts.critical})`, centerX, 935, {
+    drawText(ctx, `SLA วิกฤต (${Number(counts.critical || 0)})`, centerX, 935, {
       font: 'bold 52px "ThaiBold"',
       align: "center"
     });
 
     // 🔥 ปุ่ม 2
-    drawText(ctx, `เคสด่วน SLA ใกล้วิกฤต (${counts.warning})`, centerX, 1095, {
+   drawText(ctx, `SLA ใกล้วิกฤต (${Number(counts.warning || 0)})`, centerX, 1095, {
       font: 'bold 52px "ThaiBold"',
       align: "center"
     });
 
     // 🔥 ปุ่ม 3
-    drawText(ctx, `กำลังดำเนินการ (${counts.normal})`, centerX, 1250, {
+    drawText(ctx, `กำลังดำเนินการ (${Number(counts.normal || 0)})`, centerX, 1250, {
       font: 'bold 52px "ThaiBold"',
       align: "center"
     });

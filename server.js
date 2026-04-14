6d4f25881baf0c2a1cfd690ba6c3f3e62b19e8e7
kitnutthakorn-wq <kitnutthakorn@gmail.com>
2026-04-14 14:26:03 +0700

    Update server.js

--- server.js
+++ server.js
@@ -818,7 +818,7 @@ app.get("/imagemap/admin-case-menu-r:rev/:size", async (req, res) => {
     });
 
     // ปุ่ม 2: ดูเคสด่วน
-    drawText(ctx, `ดูเคสด่วน (${Number(urgentCounts.open_cases || 0)})`, centerX, 945, {
+    drawText(ctx, `ดูเคสด่วน (${Number(urgentCounts.open_cases || 0)})`, centerX, 975, {
       font: 'bold 52px "ThaiBold", sans-serif',
       color: "#111111",
       align: "center",

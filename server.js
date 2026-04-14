b066c9c1a84fc3bf2cd4f9f0e675bd6896a08576
kitnutthakorn-wq <kitnutthakorn@gmail.com>
2026-04-14 14:18:45 +0700

    Update server.js

--- server.js
+++ server.js
@@ -810,7 +810,7 @@ app.get("/imagemap/admin-case-menu-r:rev/:size", async (req, res) => {
     const centerX = 520;
 
     // ปุ่ม 1: ดูเคสใหม่
-    drawText(ctx, `ดูเคสใหม่ (${Number(counts.total || 0)})`, centerX, 755, {
+    drawText(ctx, `ดูเคสใหม่ (${Number(counts.total || 0)})`, centerX, 752, {
       font: 'bold 52px "ThaiBold", sans-serif',
       color: "#111111",
       align: "center",
@@ -818,7 +818,7 @@ app.get("/imagemap/admin-case-menu-r:rev/:size", async (req, res) => {
     });
 
     // ปุ่ม 2: ดูเคสด่วน
-    drawText(ctx, `ดูเคสด่วน (${Number(urgentCounts.open_cases || 0)})`, centerX, 945, {
+    drawText(ctx, `ดูเคสด่วน (${Number(urgentCounts.open_cases || 0)})`, centerX, 915, {
       font: 'bold 52px "ThaiBold", sans-serif',
       color: "#111111",
       align: "center",
@@ -826,7 +826,7 @@ app.get("/imagemap/admin-case-menu-r:rev/:size", async (req, res) => {
     });
 
     // ปุ่ม 3: เคสวันนี้
-    drawText(ctx, `เคสวันนี้ (${Number(todayCount || 0)})`, centerX, 1105, {
+    drawText(ctx, `เคสวันนี้ (${Number(todayCount || 0)})`, centerX, 1085, {
       font: 'bold 52px "ThaiBold", sans-serif',
       color: "#111111",
       align: "center",
